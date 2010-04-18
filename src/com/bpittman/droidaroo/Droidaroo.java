f178b86afdac52c8e6442ffe70e54cbfc8c76183
Brian Pittman <shweazel@gmail.com>
2010-04-18 17:30:07 -0500

    Add some layouts and intents for the other screens

--- src/com/bpittman/droidaroo/Droidaroo.java
+++ src/com/bpittman/droidaroo/Droidaroo.java
@@ -1,13 +1,57 @@
 package com.bpittman.droidaroo;
 
 import android.app.Activity;
+import android.widget.Button;
+import android.view.View;
 import android.os.Bundle;
+import android.content.Intent;
 
 public class Droidaroo extends Activity {
-    /** Called when the activity is first created. */
+    private View.OnClickListener mBandsListener = new View.OnClickListener() {
+        public void onClick(View v) {
+        	Intent myIntent = new Intent(Droidaroo.this, BandsList.class);
+        	startActivity(myIntent);
+        }
+    };
+    private View.OnClickListener mStagesListener = new View.OnClickListener() {
+        public void onClick(View v) {
+        	Intent myIntent = new Intent(Droidaroo.this, StagesList.class);
+        	startActivity(myIntent);
+        }
+    };
+    private View.OnClickListener mStarredListener = new View.OnClickListener() {
+        public void onClick(View v) {
+        	Intent myIntent = new Intent(Droidaroo.this, StarredList.class);
+        	startActivity(myIntent);
+        }
+    };
+    private View.OnClickListener mMapListener = new View.OnClickListener() {
+        public void onClick(View v) {
+        	Intent myIntent = new Intent(Droidaroo.this, Map.class);
+        	startActivity(myIntent);
+        }
+    };
+    private View.OnClickListener mTodayListener = new View.OnClickListener() {
+        public void onClick(View v) {
+        	Intent myIntent = new Intent(Droidaroo.this, TodayList.class);
+        	startActivity(myIntent);
+        }
+    };
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        Button bandsButton = (Button)findViewById(R.id.bands);
+        bandsButton.setOnClickListener(mBandsListener);
+        Button stagesButton = (Button)findViewById(R.id.stages);
+        stagesButton.setOnClickListener(mStagesListener);
+        Button starredButton = (Button)findViewById(R.id.starred);
+        starredButton.setOnClickListener(mStarredListener);
+        Button mapButton = (Button)findViewById(R.id.map);
+        mapButton.setOnClickListener(mMapListener);
+        Button todayButton = (Button)findViewById(R.id.today);
+        todayButton.setOnClickListener(mTodayListener);
+
     }
 }
