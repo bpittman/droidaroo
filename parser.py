4f182595f05634c85f9ee20b6da4f70b9affe60c
Brian Pittman <shweazel@gmail.com>
2010-05-30 19:12:01 -0500

    Use sqlite to save the parsed event data

--- parser.py
+++ parser.py
@@ -1,8 +1,29 @@
 # -*- coding: utf-8 -*-
 import demjson
+import sqlite3
 
 venues = demjson.decode(open("venues.txt").read())
 events = demjson.decode(open("events.txt").read())
 
+conn = sqlite3.connect('./eventDB')
+c = conn.cursor()
+
+# Create table
+c.execute('drop table if exists events')
+c.execute('''create table events
+(id int, venueId int,
+line1 text, line2 text,
+start text, duration text)''')
+
 for eventID in events:
-   print events[eventID]["line1"], events[eventID]["start"]
+   t = (eventID,events[eventID]["venueId"],
+        events[eventID]["line1"],events[eventID]["line2"],
+        events[eventID]["start"],events[eventID]["duration"])
+   c.execute('insert into events values (?,?,?,?,?,?)',t)
+
+c.execute('select * from events')
+for row in c:
+   print row
+
+conn.commit()
+conn.close()
